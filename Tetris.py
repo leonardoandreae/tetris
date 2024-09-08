33461a006663186c5ff8bc030af34ed199a7351c
leonardoandreae <leoandreae@gmail.com>
2024-09-08 22:29:57 +0200

    discretized lateral movement

--- Tetris.py
+++ Tetris.py
@@ -5,24 +5,34 @@ import GameParameters as par
 
 def event_handler():
     global game_running, fall_ev, tile_falling
-    # pressing the "X" button terminates the application
+
     for event in pyg.event.get():
+        # pressing the "X" button terminates the application
         if event.type == pyg.QUIT:
             game_running = False
         if event.type == fall_ev:
             tile_falling = True
+            
 
 def update_tile_position():
-    global x, y, ms_count, ms_count_prev, tile_falling
+    global x, y, ms_count, ms_count_prev, tile_falling, lateral_movement_prevented
     keys_pressed = pyg.key.get_pressed()
     
+    if (lateral_movement_prevented == True and (not keys_pressed[pyg.K_LEFT])
+            and (not keys_pressed[pyg.K_RIGHT])):
+        lateral_movement_prevented = False
+                
+    
     if (keys_pressed[pyg.K_LEFT] and (not keys_pressed[pyg.K_RIGHT])
-            and x > par.GRID_TLC_x):
+            and x > par.GRID_TLC_x and (not lateral_movement_prevented)):
         x -= par.GRID_ELEM_SIZE
+        lateral_movement_prevented = True
             
     if (keys_pressed[pyg.K_RIGHT] and (not keys_pressed[pyg.K_LEFT])
-            and x < par.GRID_TLC_x + par.GRID_ELEM_SIZE * (par.GRID_NR_OF_COLS - 1)):
+            and x < par.GRID_TLC_x + par.GRID_ELEM_SIZE * (par.GRID_NR_OF_COLS - 1)
+            and (not lateral_movement_prevented)):
         x += par.GRID_ELEM_SIZE
+        lateral_movement_prevented = True
         
     if tile_falling and y < par.GRID_TLC_y + par.GRID_ELEM_SIZE * (par.GRID_NR_OF_ROWS - 1):
         y += par.GRID_ELEM_SIZE
@@ -35,6 +45,7 @@ def draw_grid():
             # create rectangle object
             grid_element = pyg.Rect(column, row, par.GRID_ELEM_SIZE, par.GRID_ELEM_SIZE)
             pyg.draw.rect(game_window, par.BLACK, grid_element, 1)
+            
             
 def collision_detection():
     collision_detected = False
@@ -55,7 +66,7 @@ def update_scene():
     pyg.display.update()
 
 def main():
-    global game_window, BACKGROUND, tetris_logo, x, y, game_running, fall_ev, tile_falling
+    global game_window, BACKGROUND, tetris_logo, x, y, game_running, fall_ev, tile_falling, lateral_movement_prevented
     pyg.init()
     game_window = pyg.display.set_mode((par.GAME_WINDOW_WIDTH, par.GAME_WINDOW_HEIGHT))
     pyg.display.set_caption("Tetris")
@@ -68,7 +79,6 @@ def main():
     tetris_logo = pyg.image.load('assets/tetris_logo.png')
     tetris_logo = pyg.transform.scale2x(tetris_logo)
     
-
     clock = pyg.time.Clock()
 
     # initial rectangle position (top left corner)
@@ -79,6 +89,7 @@ def main():
     fall_ev = pyg.USEREVENT + 0 # event ID = 24 (up to 32, but first 23 are used by pygame already)
     pyg.time.set_timer(fall_ev, par.FALL_TIME_INTERVAL_ms)
     tile_falling = False
+    lateral_movement_prevented = False
 
     game_running = True    
     while game_running:
