da4e1e9bcde0acb1923bff0a10631e8925240351
Leonardo Andreae <leoandreae@gmail.com>
2024-12-02 16:45:43 +0100

    implemented next piece preview

--- src/GameInterface.py
+++ src/GameInterface.py
@@ -7,11 +7,11 @@ class GameInterface:
         pyg.init()
         self.game_window = pyg.display.set_mode((par.GAME_WINDOW_WIDTH, par.GAME_WINDOW_HEIGHT))
         pyg.display.set_caption("Tetris")
-        # https://www.freepik.com/icons/tetris Icon by Freepik
-        self.icon = pyg.image.load('assets/tetris_icon.png')
+        self.icon = pyg.image.load('assets/tetris_icon.png') # https://www.freepik.com/icons/tetris Icon by Freepik
         pyg.display.set_icon(self.icon)
         self.logo = pyg.image.load('assets/tetris_logo.png')
-        self.text_font = pyg.freetype.SysFont(pyg.freetype.get_default_font(), par.FONT_SIZE)
+        self.text_font_1 = pyg.freetype.SysFont(pyg.freetype.get_default_font(), par.FONT_SIZE_1)
+        self.text_font_2 = pyg.freetype.SysFont(pyg.freetype.get_default_font(), par.FONT_SIZE_2)
         pyg.mixer.music.load('assets/tetris-theme.mp3')
         self.rotation_sfx = pyg.mixer.Sound('assets/rotation.mp3')
         self.single_sfx = pyg.mixer.Sound('assets/single.mp3')
@@ -34,20 +34,20 @@ class GameInterface:
             if event.type == game_state.downwards_drop_ev:
                 tile.can_drop = True
 
-    def draw_grid(self):
+    def draw_grid(self, nr_of_rows, nr_of_cols, TLC_coords):
         # draw horizontal lines
-        for row_idx in range(0, par.GRID_NR_OF_ROWS + 1):
-            start_coords = (par.GRID_TLC_x, par.GRID_TLC_y + row_idx * par.GRID_ELEM_SIZE)
-            end_coords = (par.GRID_TLC_x + par.GRID_NR_OF_COLS * par.GRID_ELEM_SIZE, 
-                        par.GRID_TLC_y + row_idx * par.GRID_ELEM_SIZE)
+        for row_idx in range(0, nr_of_rows + 1):
+            start_coords = (TLC_coords.x, TLC_coords.y + row_idx * par.GRID_ELEM_SIZE)
+            end_coords = (TLC_coords.x + nr_of_cols * par.GRID_ELEM_SIZE, 
+                        TLC_coords.y + row_idx * par.GRID_ELEM_SIZE)
             pyg.draw.line(surface=self.game_window, color=par.BLACK, 
                         start_pos=start_coords, end_pos=end_coords,
                         width=par.GRID_THICKNESS)
         # draw vertical lines    
-        for col_idx in range(0, par.GRID_NR_OF_COLS + 1):
-            start_coords = (par.GRID_TLC_x + col_idx * par.GRID_ELEM_SIZE, par.GRID_TLC_y)
-            end_coords = (par.GRID_TLC_x + col_idx * par.GRID_ELEM_SIZE, 
-                        par.GRID_TLC_y + par.GRID_ELEM_SIZE * par.GRID_NR_OF_ROWS)
+        for col_idx in range(0, nr_of_cols + 1):
+            start_coords = (TLC_coords.x + col_idx * par.GRID_ELEM_SIZE, TLC_coords.y)
+            end_coords = (TLC_coords.x + col_idx * par.GRID_ELEM_SIZE, 
+                         TLC_coords.y + par.GRID_ELEM_SIZE * nr_of_rows)
             pyg.draw.line(surface=self.game_window, color=par.BLACK, 
                         start_pos=start_coords, end_pos=end_coords,
                         width=par.GRID_THICKNESS)
@@ -83,6 +83,18 @@ class GameInterface:
                                                 par.GRID_ELEM_SIZE,
                                                 par.TILE_COLORS[tile.type],
                                                 par.WHITE)
+                    
+    # TODO unite this function with the one above                
+    def draw_next_tile(self, tile_type):
+        configuration_matrix = par.TILE_SHAPES[tile_type][0]
+        for col in range (0, par.TILE_CONFIG_IDX_MAX):
+            for row in range (0, par.TILE_CONFIG_IDX_MAX):
+                if configuration_matrix[row][col] == 1:
+                    self.draw_block_with_borders(par.NEXT_PIECE_GRID_POS.x + par.GRID_ELEM_SIZE * col,
+                                                par.NEXT_PIECE_GRID_POS.y + par.GRID_ELEM_SIZE * row,
+                                                par.GRID_ELEM_SIZE,
+                                                par.TILE_COLORS[tile_type],
+                                                par.WHITE)
     
     def draw_scene(self, tile, game_state):
         # TODO: only draw tile and board each time not the entire thing
@@ -90,20 +102,24 @@ class GameInterface:
         self.game_window.fill(par.GREY)
         self.game_window.blit(self.logo, par.LOGO_POS)
 
-        score_surface, _ = self.text_font.render(f'Score:  {game_state.score}', par.WHITE)
-        level_surface, _ = self.text_font.render(f'Level:   {game_state.level}', par.WHITE)
-        lines_surface, _ = self.text_font.render(f'Lines:   {game_state.lines}', par.WHITE)
+        next_piece_text_surface, _ = self.text_font_2.render(f'Next Piece:', par.WHITE)
+        score_text_surface, _ = self.text_font_1.render(f'Score:  {game_state.score}', par.WHITE)
+        level_text_surface, _ = self.text_font_1.render(f'Level:   {game_state.level}', par.WHITE)
+        lines_text_surface, _ = self.text_font_1.render(f'Lines:   {game_state.lines}', par.WHITE)
 
-        y_level = par.STATS_POS[1] + self.text_font.get_sized_height() + 10
+        y_level = par.STATS_POS[1] + self.text_font_1.get_sized_height() + 10
         x_level = par.STATS_POS[0]
-        y_lines = y_level + self.text_font.get_sized_height() + 10
+        y_lines = y_level + self.text_font_1.get_sized_height() + 10
         x_lines = x_level
 
-        self.game_window.blit(score_surface, par.STATS_POS)
-        self.game_window.blit(level_surface, (x_level, y_level))
-        self.game_window.blit(lines_surface, (x_lines, y_lines))
+        self.game_window.blit(score_text_surface, par.STATS_POS)
+        self.game_window.blit(level_text_surface, (x_level, y_level))
+        self.game_window.blit(lines_text_surface, (x_lines, y_lines))
+        self.game_window.blit(next_piece_text_surface, par.NEXT_PIECE_TEXT_POS)
         
-        self.draw_grid()
+        self.draw_grid(4, 4, par.NEXT_PIECE_GRID_POS)
+        self.draw_next_tile(tile.next_type)
+        self.draw_grid(par.GRID_NR_OF_ROWS, par.GRID_NR_OF_COLS, pyg.Vector2(par.GRID_TLC_x, par.GRID_TLC_y))
         self.draw_board(game_state)
         self.draw_tile(tile) 
         # After calling the drawing functions to make the display Surface object look the way you want, you must call this to make the display Surface actually appear on the user’s monitor.
