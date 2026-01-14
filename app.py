a09b09ce7aecce991e71c80410fcc3d16860f134
Leonardo Andreae <leoandreae@gmail.com>
2026-01-14 16:07:49 +0100

    handle pyinstaller vs normal exec

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import sys, os
-sys.path.append(os.path.join(sys.path[0], 'src'))
+
+# Handle PyInstaller vs normal execution
+if getattr(sys, "frozen", False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(os.path.join(base_path, "src"))
+
 import parameters as par
 from interface import *
 
